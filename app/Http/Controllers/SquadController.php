c1b571387fdf84665010014ead04ef47513f404c
António Camarão <camaraoacsn@gmail.com>
2023-05-12 09:34:05 +0100

    Squads can now be created and seen Successfully

--- app/Http/Controllers/SquadController.php
+++ app/Http/Controllers/SquadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Squad;
 use App\Http\Requests\StoreSquadRequest;
 use App\Http\Requests\UpdateSquadRequest;
@@ -15,21 +16,24 @@ class SquadController extends Controller
      */
     public function index()
     {
-        try{
-            return response()->json(Squad::all(), 200);
-        } catch (\Exception $exception){
-            return response()->json(['error'=>$exception],500);
-        }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        try {
+            $squads = Squad::with('users')->get();
+            $response = $squads->map(function ($squad) {
+                return [
+                    'id' => $squad->id,
+                    'squadName' => $squad->squadName,
+                    'reference' => $squad->reference,
+                    'user_ids' => $squad->users->pluck('id')->toArray(),
+                    'deleted_at' => $squad->deleted_at,
+                    'created_at' => $squad->created_at,
+                    'updated_at' => $squad->updated_at,
+                ];
+            });
+        
+            return response()->json($response, 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception], 500);
+        }        
     }
 
     /**
@@ -40,7 +44,35 @@ class SquadController extends Controller
      */
     public function store(StoreSquadRequest $request)
     {
-        //
+        // Validate and store the squad data
+        $validatedData = $request->validate([
+            'squadName' => 'required',
+            'reference' => 'required',
+        ]);
+
+        $squad = Squad::create($validatedData);
+
+        // Link users to the squad if provided
+        $user_ids = $request->input('user_id');
+        if ($user_ids) {
+            $users = User::whereIn('id', $user_ids)->get();
+            $squad->users()->attach($users);
+        }
+
+        // Fetch the updated squad data with the user IDs
+        $squad->load('users');
+
+        // Prepare the response data
+        $response = [
+            'squadName' => $squad->squadName,
+            'reference' => $squad->reference,
+            'user_ids' => $squad->users->pluck('id')->toArray(),
+            'updated_at' => $squad->updated_at,
+            'created_at' => $squad->created_at,
+            'id' => $squad->id,
+        ];
+
+        return response()->json($response);
     }
 
     /**
@@ -50,17 +82,6 @@ class SquadController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Squad $squad)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Squad  $squad
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Squad $squad)
     {
         //
     }
