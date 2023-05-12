636818aa176a4b869dfe050bd00f06a5ed1beb4a
António Camarão <camaraoacsn@gmail.com>
2023-05-12 09:33:43 +0100

    Corrected the name of the intermediate table

--- database/migrations/2023_05_10_140027_create_squad_user_table.php
+++ database/migrations/2023_05_10_140027_create_squad_user_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user_squad', function (Blueprint $table) {
+        Schema::create('squad_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
             $table->foreignId('squad_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_squad');
+        Schema::dropIfExists('squad_user');
     }
 };
