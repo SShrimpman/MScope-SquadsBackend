b9eba0f8cc53d2ed5a0153867b6ab8fae636d6b6
António Camarão <camaraoacsn@gmail.com>
2023-05-15 11:58:47 +0100

    Created new login function to use in the Frontend

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Custom\Jwt;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -9,20 +10,51 @@ use App\Http\Middleware\Authenticate;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
-    {
-        $credentials = $request->only('username', 'password');
+    // LOGIN FUNCTION THAT I USED TO COMMUNICATE WITH POSTMAN 
 
-        if (Auth::attempt($credentials)) {
-            // Authentication successful
-            $user = Auth::user();
-            $token = $user->createToken('token-name')->plainTextToken;
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->only('username', 'password');
 
-            return response()->json(['token' => $token]);
-        } else {
-            // Authentication failed
-            return response()->json(['error' => 'Username or Password is incorrect'], 401);
+    //     if (Auth::attempt($credentials)) {
+    //         // Authentication successful
+    //         $user = Auth::user();
+    //         $token = $user->createToken('token-name')->plainTextToken;
+
+    //         return response()->json(['token' => $token]);
+    //     } else {
+    //         // Authentication failed
+    //         return response()->json(['error' => 'Username or Password is incorrect'], 401);
+    //     }
+    // }
+
+    // LOGIN FUNCTION THAT I USE TO COMMUNICATE WITH THE FRONTEND
+
+    public function login(Request $request){
+        $user = User::where('username', $request->username)->first();
+
+        if(!$user) {
+           return response()->json('User not found!', 401);
         }
+
+        if(!password_verify($request->password, $user->password)){
+           return response()->json('Password is Invalid!', 401);
+        }
+
+        $token = Jwt::create($user);
+
+        return response()->json([
+            'token' => $token,
+            'user' => [
+                'firstName' => $user->firstName,
+                'lastName' => $user->lastName,
+                'role' => $user->role->name
+            ]
+        ]);
+    }
+
+    public function verify(){
+        return Jwt::validate();
     }
 
     public function logout(Request $request)
